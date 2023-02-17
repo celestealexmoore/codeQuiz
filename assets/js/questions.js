d4e7fcc0afc2e4e95e23af9cb11a567b600a5b31
cMoore <celestealexmoore@gmail.com>
2023-02-17 11:36:03 -0600

    changed around file struture for multiple js & css files, completed content for questions.js and imported.

---
+++ assets/js/questions.js
@@ -0,0 +1,62 @@
+export const quizQuestions = [
+  {
+    id: 0,
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: ["A. <script>", "B. <js>", "C. <javascript>", "D. <scripting>"],
+    correctAnswer: 0 // goes by index
+  },
+  {
+    id: 1,
+    question: 'Where is the correct place to insert a JavaScript file?',
+    answers: ["A. The <link> section.", "B. The <body> section.", "C. The <footer> section.", "D. The <header> section."],
+    correctAnswer: 1,
+  },
+  {
+    id: 2,
+    question: `How do you write "Hello World" in an alert box?`,
+    answers: [`A. msg("Hello World")`,`"B. alertBox("Hello World")`, `C. msgBox("Hello World")`, `D. alert("Hello World")`],
+    correctAnswer: 3
+  },
+  {
+    id: 3,
+    question: "How do you create a function in JavaScript?",
+    answers: ["A. function = myFunction()", "B. function myFunction()", "C. function:myFunction", "D. myFunction() => function"],
+    correctAnswer: 1
+  },
+  {
+    id: 4,
+    question: `How do you call a function named "myFunction"?`,
+    answers: ["A. myFunction()", "B. export myFunction", "C. call myFunction()", "D. call function myFunction()"],
+    correctAnswer: 0
+  },
+  {
+    id: 5,
+    question: "How to write an IF statement in JavaScript?",
+    answers: ["A. if i = 5", "B. if(i == 5)", "C. if i == 5 then", "D. if i = 5 then"],
+    correctAnswer: 1
+  },
+  {
+    id: 6,
+    question: "How does a WHILE loop start?",
+    answers: ["A. while i <= 10", "B. while(i <=10; i++)", "C. while i = 1 to 10", "D. while(i <= 10)"],
+    correctAnswer: 3
+  },
+  {
+    id: 7,
+    question: "How does a FOR loop start?",
+    answers: ["A. for(i <= 5; i++)", "B. for i = 1 to 5", "C. for(i = 0; i <= 5; i++)", "D. for(i = 0; i <= 5)"],
+    correctAnswer: 2
+  },
+  {
+    id: 8,
+    question: "How can you add a one-line comment in a JavaScript?",
+    answers: ["A. <-- This is a comment--> ", "B. // This is a comment", `C. 'This is a comment`, "D. *This is a comment*"],
+    correctAnswer: 1
+  },
+  {
+    id: 9,
+    question: "How do you round the number 7.25, to the nearest integer?",
+    answers: ["A. Math.round(7.25) ", "B. round(7.25)", "C. rnd(7.25)", "D. Math.rnd(7.25)"],
+    correctAnswer: 0
+  },
+];
