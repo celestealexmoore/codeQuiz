fc8312c14a677df87ecb79d8cb1a067567cb756a
Celeste A. Moore <77650238+celestealexmoore@users.noreply.github.com>
2023-02-20 15:39:42 -0600

    Function to compare user answer to correct answer, question/answer/feedback message appended

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,11 @@ const startQuizBtn = document.getElementById("startQuizBtn");
 const quizScreen = document.getElementById("quizScreen");
 const timer = document.getElementById("timer");
 const quizQuestion = document.getElementById("quizQuestion");
-const quizChoices = document.getElementById("quizChoices");
+const answerChoicesParent = document.getElementById("answerChoicesParent");
 const indResult = document.getElementById("indResult");
 const resultScreen = document.getElementById("resultScreen");
 
-let countdown = 5;
+let countdown = 60;
 let timerVar;
 
 quizScreen.style.display = "none";
@@ -17,28 +17,66 @@ resultScreen.style.display = "none";
 startQuizBtn.onclick = () => {
   startScreen.style.display = "none";
   quizScreen.style.display = "flex";
- timerVar = setInterval(timerElapse, 1000);
+  timerVar = setInterval(timerElapse, 1000);
+  newQuestion();
 };
 
 /* timer function: */
-function timerElapse() {
+let timerElapse = () => {
   timer.innerText = countdown--;
-  countdown === -1 ? clearInterval(timerVar) : null;
-}
+
+  if (countdown === -1) {
+    clearInterval(timerVar),
+      (quizBody.style.display = "none"),
+      (resultScreen.style.display = "flex");
+  }
+};
+
+let newQuestion = () => {
+  let a, b, c, d;
+  let i = 0;
+
+  for (i = i; i < 1; i++) {
+    // console.log(i);
+    // Question Title
+    quizQuestion.innerHTML = quizQuestions[i].question;
+
+    //Answer Buttons
+    a = document.createElement("button");
+    a.innerHTML = quizQuestions[i].answers[0];
+    b = document.createElement("button");
+    b.innerHTML = quizQuestions[i].answers[1];
+    c = document.createElement("button");
+    c.innerHTML = quizQuestions[i].answers[2];
+    d = document.createElement("button");
+    d.innerHTML = quizQuestions[i].answers[3];
+
+    answerChoicesParent.append(a, b, c, d);
+
+    let children = answerChoicesParent.childNodes;
+    children.forEach((child) => {
+      child.classList.add("quizChoices");
+      child.setAttribute("type", "submit");
+      child.setAttribute("id", i++);
+      child.addEventListener("click", function () {
+        if (child.id == quizQuestions[0].correctAnswer) {
+          indResult.innerText = "That's Correct!";
+          //   i += 1;
+          //   newQuestion()
+        } else {
+          indResult.innerText = "That's Incorrect.";
+          countdown = countdown - 10;
+          //   i += 1;
+        }
+      });
+    });
+  }
+};
 
 /* 
 
-startButton on click:
-
-start timer countdown from 60 in one-second intervals.
-stop timer when it gets to 0 and set #startScreen to display: none,
-swap resultScreen to display: block.
-
 #saveInitialsBtn onclick should store #initialInputBox.value to local storage
 swap to highscores.html
-
-
-
 
 Example of swapping HTMLs:
 var button = document.getElementById("button1");
@@ -47,5 +85,5 @@ console.log('I just moved a page')
 window.location.href = "#your-url-here";
 });
 
-
+each time a question is answered and feedback provided, run quiz.
 */
